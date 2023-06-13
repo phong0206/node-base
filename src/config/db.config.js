4799e12fd33d4b30a42087f32d6f2d9a143a480f
Phong <vanphong02062002@gmail.com>
2023-06-13 17:48:48 +0700

    f

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "localhost",
+  HOST: "127.0.0.1",
   PORT: 27017,
   DB: "phongnv_db",
 };
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,60 +6,49 @@ const Role = db.role;
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
-exports.signup = (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8), // hash password with 8 cost factor
-  });
-
-  user.save((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    console.log(username, email, password);
+    //input validation
+    if (!username || !email || !password) {
+      return res.status(400).send( req.body.username + " is not a validf");
     }
+    //hash password asynchronously
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    const user = new User({
+      username: username,
+      email: email,
+      password: hashedPassword,
+    });
+
+    let savedUser = await user.save();
 
     if (req.body.roles) {
-      Role.find(
-        {
-          name: { $in: req.body.roles },
-        },
-        (err, roles) => {
-          if (err) {
-            res.status(500).send({ message: err });
-            return;
-          }
-
-          user.roles = roles.map((role) => role._id);
-          user.save((err) => {
-            if (err) {
-              res.status(500).send({ message: err });
-              return;
-            }
-
-            res.send({ message: "User was registered successfully!" });
-          });
-        }
-      );
+      const roles = await Role.find({ name: { $in: req.body.roles } });
+      if (roles.length == 0) {
+        throw new Error("Roles not found");
+      }
+      //use savedUser instead of user
+      savedUser.roles = roles.map((role) => role._id);
+      await savedUser.save();
     } else {
-      Role.findOne({ name: "user" }, (err, role) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
-        user.roles = [role._id];
-        user.save((err) => {
-          if (err) {
-            res.status(500).send({ message: err });
-            return;
-          }
-
-          res.send({ message: "User was registered successfully!" });
-        });
-      });
+      let role = await Role.findOne({ name: "user" });
+      if (!role) {
+        throw new Error("Role not found");
+      }
+      //use savedUser instead of user
+      savedUser.roles = [role._id];
+      await savedUser.save();
     }
-  });
+
+    res.status(201).json({ message: "User was registered successfully!" });
+  } catch (err) {
+    //handle errors consistently and gracefully
+    console.error(err);
+    return res.status(500).json({ message: err.message });
+  }
 };
 
 exports.signin = (req, res) => {
@@ -77,24 +66,13 @@ exports.signin = (req, res) => {
         return res.status(404).send({ message: "User Not found." });
       }
 
-      var passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
+      const authorities = user.roles.map(
+        (role) => `ROLE_${role.name.toUpperCase()}`
       );
 
-      if (!passwordIsValid) {
-        return res.status(401).send({ message: "Invalid Password!" });
-      }
-
-      var token = jwt.sign({ id: user.id }, config.secret, {
+      const token = jwt.sign({ id: user.id }, config.secret, {
         expiresIn: 86400, // 24 hours
       });
-
-      var authorities = [];
-
-      for (let i = 0; i < user.roles.length; i++) {
-        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-      }
 
       req.session.token = token;
 
